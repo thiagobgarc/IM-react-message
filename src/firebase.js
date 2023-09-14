4c656c2cc7593b3f09b8dcc532aba1d37302e8a5
Thiago Bueno Garcia <117615425+thiagobgarc@users.noreply.github.com>
2023-09-13 21:34:13 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDtrFGUiOLg5QnUhH2HqnJgct_IIRdOQLM",
-  authDomain: "imapps-6ba82.firebaseapp.com",
-  projectId: "imapps-6ba82",
-  storageBucket: "imapps-6ba82.appspot.com",
-  messagingSenderId: "499163170717",
-  appId: "1:499163170717:web:87373f8f8411c7d1801174",
-  measurementId: "G-1Z0VJG61EG"
-};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
